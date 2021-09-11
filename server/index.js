cbc72e5c5fd0360b8d941f5cd7b7323127dac5b4
Prionti Nasir <priontin@spotify.com>
2021-09-11 18:12:35 -0400

    created students table

--- server/index.js
+++ server/index.js
@@ -3,21 +3,36 @@ require('dotenv').config({ path: '../process.env' });
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: process.env.USERNAME,
-    host: process.env.HOSTNAME,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.PORTNUMBER,
-  })
+user: process.env.USERNAME,
+host: process.env.HOSTNAME,
+database: process.env.DATABASE,
+password: process.env.PASSWORD,
+port: process.env.PORTNUMBER,
+})
 
-const getUsers = (request, response) => {
-    pool.query('SELECT * FROM students ORDER BY id ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-  }
+pool.query("CREATE TABLE IF NOT EXISTS students(uid SERIAL PRIMARY KEY, "
+    + " firstName VARCHAR(20), lastName VARCHAR(20), email VARCHAR(15), year INTEGER, "
+    + " major TEXT, college TEXT, registrationDate DATE DEFAULT CURRENT_DATE, balance MONEY, hold BOOLEAN DEFAULT FALSE);", (err, res) => {
+    console.log(err, res);
+    pool.query(
+        "INSERT INTO students(uid, firstName, lastName, email, year, major, college, balance) " +
+        " VALUES(640000337, 'John', 'Smith', 'js7708@rit.edu', 4, 'Software Engineering', " +
+        " 'Golisano College of Computing and Information Sciences', 0.00)",
+        (err, res) => {
+            console.log(err, res);
+        }
+        );
+});
+
+
+const getStudents = (request, response) => {
+pool.query('SELECT * FROM students ORDER BY id ASC', (error, results) => {
+    if (error) {
+    throw error
+    }
+    response.status(200).json(results.rows)
+})
+}
 
 const app = express();
 const port = 3000;
@@ -25,10 +40,10 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/', (request, response) => {
-    response.json({ info: 'This is our empty homepage.'})
-  })
-  
-app.get('/users', getUsers)
+response.json({ info: 'This is our empty homepage.'})
+})
+
+app.get('/users', getStudents)
 
 app.listen(port, () => {
 console.log(`App running on port ${port}.`)
