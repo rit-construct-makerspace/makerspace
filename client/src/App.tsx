533d276d06194526e7c3a0404b3b2893219a3aef
MatthewGalan <mattg54321@gmail.com>
2021-09-13 14:10:38 -0400

    add prettier, react-router, and styled-components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+  return <div>very cool much react</div>;
 }
 
 export default App;
