f7b6d39fcd63bc0e27ba582d22ec88d927a891be
MatthewGalan <mattg54321@gmail.com>
2021-09-13 21:16:08 -0400

    add reusable button component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Button, { ButtonType } from "./components/common/Button";
 
 function App() {
-  return <div>very cool much react</div>;
+  return (
+    <div>
+      <Button label="Approve" onClick={() => alert("hello")} />
+      <Button label="Scary Button" buttonType={ButtonType.Destructive} />
+      <Button label="Disabled Button" disabled />
+    </div>
+  );
 }
 
 export default App;
