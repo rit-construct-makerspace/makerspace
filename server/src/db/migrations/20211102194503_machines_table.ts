d68d6ea1439cd2eb04e0e87930c56df1b6020606
smellslikekeenspirit <pdn3628@rit.edu>
2021-11-04 13:54:49 -0400

    resolved local docker issues + machine migration

---
+++ server/src/db/migrations/20211102194503_machines_table.ts
@@ -0,0 +1,50 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+
+    knex.schema.hasTable('MachineFamilies').then(function(exists) {
+        if (!exists) {
+          return knex.schema.createTable('MachineFamilies', function(t) {
+            t.increments('id');
+            t.string('name', 50);
+            t.text('description');
+            t.integer('training_module');
+            t.foreign('training_module').references('id').inTable('TrainingModule');
+          });
+        }
+    });
+
+    knex.schema.hasTable('Machines').then(function(exists) {
+        if (!exists) {
+          return knex.schema.createTable('Machines', function(t) {
+            t.increments('id');
+            t.string('name', 50);
+            t.string('category', 50);
+            t.foreign('category').references('name').inTable('MachineFamilies');
+            t.string('room', 5);
+            t.date('date_added').defaultTo(knex.fn.now());
+            t.boolean('reserved').defaultTo(false);
+            t.integer('reserved_by');
+            t.foreign('reserved_by').references('id').inTable('students');
+          });
+        }
+    });
+    
+}
+
+export async function down(knex: Knex): Promise<void> {
+
+  knex.schema.hasTable('MachineFamilies').then(function(exists) {
+    if (exists) {
+      return knex.schema.dropTable('MachineFamilies');
+    }
+  });
+
+  knex.schema.hasTable('Machines').then(function(exists) {
+    if (exists) {
+      return knex.schema.dropTable('Machines');
+    }
+  });
+
+}
