4537caec2b4bf4d9141e518099cef1c94c208f70
Patrick Wilbur <pjw9963@rit.edu>
2021-11-23 15:10:49 -0500

    updated schema to have requested changes in PR

--- server/src/schemas/storeFrontSchema.ts
+++ server/src/schemas/storeFrontSchema.ts
@@ -2,73 +2,72 @@ import { gql } from "apollo-server-express";
 
 export const StoreFrontTypeDefs = gql`
 
-	scalar Date
+  scalar Date
 
-	type InventoryItem {
-		id: ID!;
-		image: String;
-		name: String;
-		labels: [String];
-		unit: String;
-		pluralUnit: String;
-		count: Int;
-		pricePerUnit: Float;
-	}
+  type InventoryItem {
+    id: ID!;
+    image: String;
+    name: String;
+    labels: [String];
+    unit: String;
+    pluralUnit: String;
+    count: Int;
+    pricePerUnit: Float;
+  }
 
-	input InventoryItemInput {
-		image: String;
-		name: String;
-		labels: [String];
-		unit: String;
-		pluralUnit: String;
-		count: Int;
-		pricePerUnit: Float;
-	}
+  input InventoryItemInput {
+    image: String;
+    name: String;
+    labels: [String];
+    unit: String;
+    pluralUnit: String;
+    count: Int;
+    pricePerUnit: Float;
+  }
 
-	type PurchaseOrderItem {
-		id: ID!;
-		item: InventoryItem;
-		count: Int;
-	}
+  type PurchaseOrderItem {
+    id: ID!;
+    item: InventoryItem;
+    count: Int;
+  }
 
-	type PurchaseOrder {
-		id: ID!;
-		creator: Person;
-		createDate: Date;
-		expectedDeliveryDate: Date;
-		items: [PurchaseOrderItem];
-		attachments: [String];
-	}
+  type PurchaseOrder {
+    id: ID!;
+    creator: Person;
+    createDate: Date;
+    expectedDeliveryDate: Date;
+    items: [PurchaseOrderItem];
+    attachments: [String];
+  }
 
-	input PurchaseOrderInput {
-		creator: Person;
-		createDate: Date;
-		expectedDeliveryDate: Date;
-		items: [PurchaseOrderItem];
-		attachments: [String];
-	}
+  input PurchaseOrderInput {
+    creator: Person;
+    expectedDeliveryDate: Date;
+    items: [PurchaseOrderItem];
+    attachments: [String];
+  }
 
-	# might belong in a different file but just gonna throw it here for now
-	type Person {
-		id: ID!;
-		name: String;
-		avatarHref: String;
-	}
+  # might belong in a different file but just gonna throw it here for now
+  type Person {
+    id: ID!;
+    name: String;
+    image: String;
+  }
 
-	type Query {
-		InventoryItems: [InventoryItem]
-		PurchaseOrders: [PurchaseOrder]
-	}
+  type Query {
+    InventoryItems: [InventoryItem]
+    PurchaseOrders: [PurchaseOrder]
+  }
 
-	type Mutation {
-		createPurchaseOrder(PO: PurchaseOrderInput): PurchaseOrder
-		addItemToPurchaseOrder(POId: ID!, itemId: ID!, count: Int): PurchaseOrder
-		removeItemToPurchaseOrder(POId: ID!, itemId: ID!, count: Int): PurchaseOrder
-		updateItemAmountInPurchaseOrder(POId: ID!, POItemId: ID!, count: Int!): PurchaseOrder
-		addAttachmentsToPurchaseOrder(POId: ID!, attachments: [String]): PurchaseOrder
-		removeAttachmentFromPurchaseOrder(POId: ID!, attachment: String): PurchaseOrder
-		addItemToInventory(item: InventoryItemInput): InventoryItem
-		addItemAmount(itemId: ID!, count: Int!): InventoryItem
-		removeItemAmount(itemId: ID!, count: Int!): InventoryItem
-	}
+  type Mutation {
+    createPurchaseOrder(PO: PurchaseOrderInput): PurchaseOrder
+    addItemToPurchaseOrder(POId: ID!, itemId: ID!, count: Int): PurchaseOrder
+    removeItemFromPurchaseOrder(POId: ID!, itemId: ID!, count: Int): PurchaseOrder
+    updateItemAmountInPurchaseOrder(POId: ID!, POItemId: ID!, count: Int!): PurchaseOrder
+    addAttachmentsToPurchaseOrder(POId: ID!, attachments: [String]): PurchaseOrder
+    removeAttachmentFromPurchaseOrder(POId: ID!, attachment: String): PurchaseOrder
+    addItemToInventory(item: InventoryItemInput): InventoryItem
+    addItemAmount(itemId: ID!, count: Int!): InventoryItem
+    removeItemAmount(itemId: ID!, count: Int!): InventoryItem
+  }
 `;
