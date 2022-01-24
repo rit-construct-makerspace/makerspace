a160dcfd38825dcc3546f57f2b873766fd194821
MatthewGalan <mattg54321@gmail.com>
2022-01-24 18:26:42 -0500

    wrap up maker training module page

--- client/src/pages/maker/training/TrainingPage.tsx
+++ client/src/pages/maker/training/TrainingPage.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import Page from "../../Page";
 import Explainer from "./Explainer";
 import PageSectionHeader from "../../../common/PageSectionHeader";
+import MakerTrainingModules from "../../../test_data/MakerTrainingModules";
+import AvailableModuleThumbnail from "./AvailableModuleThumbnail";
+import CompletedModuleThumbnail from "./CompletedModuleThumbnail";
+import { Stack } from "@mui/material";
 
 interface TrainingPageProps {}
 
@@ -9,10 +13,32 @@ export default function TrainingPage({}: TrainingPageProps) {
   return (
     <Page title="Training">
       <Explainer />
+
       <PageSectionHeader>Available training modules</PageSectionHeader>
-      coming soon
+
+      <Stack direction="row" flexWrap="wrap">
+        {MakerTrainingModules.available.map((m) => (
+          <AvailableModuleThumbnail
+            key={m.id}
+            title={m.title}
+            image={m.image}
+            estimatedDuration={m.estimatedDuration}
+          />
+        ))}
+      </Stack>
+
       <PageSectionHeader>Completed training modules</PageSectionHeader>
-      also coming soon
+
+      <Stack direction="row" flexWrap="wrap">
+        {MakerTrainingModules.completed.map((m) => (
+          <CompletedModuleThumbnail
+            key={m.id}
+            title={m.title}
+            image={m.image}
+            completionDate={m.completionDate}
+          />
+        ))}
+      </Stack>
     </Page>
   );
 }
