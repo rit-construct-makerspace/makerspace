930d82aeb81e75344eaa1be122574272e088dbca
pdn3628 <pdn3628@g.rit.edu>
2022-01-26 03:02:14 -0500

    create holds schema

---
+++ server/src/schemas/holdsSchema.ts
@@ -0,0 +1,37 @@
+import { gql } from "apollo-server-express";
+
+export const HoldsTypeDefs = gql`
+
+  enum HoldType {
+    INCOMPLETE_TRAINING
+    SAFETY_VIOLATION
+    BALANCE_DUE
+  }
+
+  type Hold {
+    id: ID!
+    type: HoldType!
+    description: String
+    createdOn: Date
+    removedOn: Date
+    userId: User!
+  }
+
+  input HoldInput {
+    id: Int!
+    type: HoldType!
+    description: String
+    createdOn: Date
+    removedOn: Date
+    userId: Int!
+  }
+
+  type Query {
+      holds: [Hold]
+  }
+
+  type Mutation {
+    placeHold(hold: HoldInput): Hold
+    removeHold(holdId: ID!): Hold
+  }
+`;
