1790033c2fc3569aa5f297be4c9c3e4ce99ed6a7
Patrick <pjw9963@g.rit.edu>
2022-02-03 11:47:52 -0500

    person model was just wrong

--- server/src/models/store/person.ts
+++ server/src/models/store/person.ts
@@ -3,6 +3,5 @@ import { InventoryItem } from "./inventoryItem";
 // this class might/should be replaced with an actual user class
 export interface Person {
     id: number;
-    item: InventoryItem;
-    count: number;
+    name: string;
 }
