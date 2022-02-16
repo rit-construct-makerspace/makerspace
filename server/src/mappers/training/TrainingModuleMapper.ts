d98122a9461e58444497f2d06d150bbbd83cd837
pdn3628 <pdn3628@g.rit.edu>
2022-02-15 23:04:37 -0500

    training module mapper + model

---
+++ server/src/mappers/training/TrainingModuleMapper.ts
@@ -0,0 +1,16 @@
+import { TrainingModule } from "../../models/training/trainingModule";
+
+export function trainingModulesToDomain(raw: any): TrainingModule[] {
+  return raw.map((i: any) => {
+    return singleTrainingModuleToDomain(i);
+  })
+}
+
+export function singleTrainingModuleToDomain(raw: any): TrainingModule | null {
+  if (raw === undefined || raw === null) return null;
+  const value: TrainingModule = {
+    id: raw.id,
+    name: raw.name,
+  }
+  return value;
+}
