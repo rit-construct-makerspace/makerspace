8b08326c781fecda8c4d53042fd25919f199f952
pdn3628 <pdn3628@g.rit.edu>
2022-02-24 18:31:49 -0500

    updated reservations tables

--- server/src/db/migrations/20211102194503_machines_table.ts
+++ server/src/db/migrations/20211102194503_machines_table.ts
@@ -45,11 +45,26 @@ export async function up(knex: Knex): Promise<void> {
       if (!exists) {
         return knex.schema.createTable('Reservations', function (t) {
           t.increments('id');
-          t.integer('userId').references('id').inTable('Users');
-          t.integer('equipmentId').references('id').inTable('Equipment');
-          t.timestamp('createdAt').defaultTo(knex.fn.now());
+          t.integer('creator').references('id').inTable('Users');
+          t.integer('maker').references('id').inTable('Users');
+          t.integer('labbie').references('id').inTable('Users');
+          t.timestamp('createDate').defaultTo(knex.fn.now());
           t.time('startTime');
           t.time('endTime');
+          t.integer('equipment').references('id').inTable('Equipment');
+          t.enu('status', ['PENDING', 'CONFIRMED', 'CANCELLED']);
+          t.timestamp('lastUpdated').defaultTo(knex.fn.now());
+        });
+      }
+    }).then(async () => {
+      const exists = await knex.schema.hasTable('ReservationEvents');
+      if (!exists) {
+        return knex.schema.createTable('ReservationEvents', function (t) {
+          t.increments('id');
+          t.enu('eventType', ['COMMENT', 'ASSIGNMENT', 'CONFIRMATION', 'CANCELLATION']);
+          t.integer('user').references('id').inTable('Users');
+          t.timestamp('dateTime').defaultTo(knex.fn.now());
+          t.string('payload', 500);
         });
       }
     });
