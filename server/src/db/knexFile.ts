e869fc88d6b8a1bbb96199778e887e9fd184db31
MatthewGalan <mattg54321@gmail.com>
2022-03-05 13:13:59 -0500

    add pipeline knex config

--- server/src/db/knexFile.ts
+++ server/src/db/knexFile.ts
@@ -1,11 +1,29 @@
-require('dotenv').config({path:__dirname+'/./../../.env'})
+require("dotenv").config({ path: __dirname + "/./../../.env" });
 // Update with your config settings.
 module.exports = {
-
   development: {
     client: "pg",
     connection: {
       host: "localhost",
+      user: process.env.POSTGRES_USER,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+      port: 5432,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "migrations",
+    },
+  },
+
+  pipeline: {
+    client: "pg",
+    connection: {
+      host: "postgres",
       user: process.env.POSTGRES_USER,
       database: process.env.POSTGRES_DB,
       password: process.env.POSTGRES_PASSWORD,
@@ -52,5 +70,4 @@ module.exports = {
       directory: "seeds",
     },
   },
-
 };
