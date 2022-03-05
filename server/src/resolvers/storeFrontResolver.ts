5a2fbcf16e6326ec2b37dbe90d4b49fab2e52971
MatthewGalan <mattg54321@gmail.com>
2022-03-05 15:52:09 -0500

    declassify inventory and label repos

--- server/src/resolvers/storeFrontResolver.ts
+++ server/src/resolvers/storeFrontResolver.ts
@@ -1,64 +1,61 @@
 import { InventoryItemInput } from "../models/store/inventoryItemInput";
-import { InventoryRepository } from "../repositories/Store/inventoryRepository";
-import { LabelRepository } from "../repositories/Store/labelRepository";
-
-const inventoryRepo = new InventoryRepository();
-const labelRepo = new LabelRepository();
+import * as InventoryRepo from "../repositories/Store/inventoryRepository";
+import * as LabelRepo from "../repositories/Store/labelRepository";
 
 const StorefrontResolvers = {
   Query: {
     InventoryItems: async (_: any, args: any, context: any) => {
-      return await inventoryRepo.getItems();
+      return await InventoryRepo.getItems();
     },
 
     InventoryItem: async (_: any, args: { Id: number }, context: any) => {
-      return await inventoryRepo.getItemById(args.Id);
+      return await InventoryRepo.getItemById(args.Id);
     },
 
     Labels: async () => {
-      return await labelRepo.getAllLabels();
+      return await LabelRepo.getAllLabels();
     },
   },
 
   InventoryItem: {
     labels: (parent: any) => {
-      return inventoryRepo.getLabels(parent.id);
+      return InventoryRepo.getLabels(parent.id);
     },
   },
 
   Mutation: {
     createInventoryItem: async (_: any, args: { item: InventoryItemInput }) => {
-      return await inventoryRepo.addItem(args.item);
+      return await InventoryRepo.addItem(args.item);
     },
 
     updateInventoryItem: async (
       _: any,
       args: { itemId: number; item: InventoryItemInput }
     ) => {
-      return await inventoryRepo.updateItemById(args.itemId, args.item);
+      return await InventoryRepo.updateItemById(args.itemId, args.item);
     },
 
     addItemAmount: async (_: any, args: { itemId: number; count: number }) => {
-      return inventoryRepo.addItemAmount(args.itemId, args.count);
+      return InventoryRepo.addItemAmount(args.itemId, args.count);
     },
 
     removeItemAmount: async (
       _: any,
       args: { itemId: number; count: number }
     ) => {
-      return inventoryRepo.addItemAmount(args.itemId, args.count * -1);
+      return InventoryRepo.addItemAmount(args.itemId, args.count * -1);
     },
 
     deleteInventoryItem: async (_: any, args: { itemId: number }) => {
-      return inventoryRepo.deleteItemById(args.itemId);
+      return InventoryRepo.deleteItemById(args.itemId);
     },
 
     createLabel: async (_: any, args: { label: string }) => {
-      await labelRepo.addLabel(args.label);
+      await LabelRepo.addLabel(args.label);
     },
 
     deleteLabel: async (_: any, args: { label: string }) => {
-      await labelRepo.deleteLabel(args.label);
+      await LabelRepo.deleteLabel(args.label);
     },
   },
 };
