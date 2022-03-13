2dbee1effaf39fbc14a450c97f3edae0e562325c
Patrick Wilbur <pjw9963@rit.edu>
2022-03-13 18:13:41 -0400

    that shouldnt be nullable

--- server/src/db/migrations/20220307225738_add-order-to-training-mod-items.ts
+++ server/src/db/migrations/20220307225738_add-order-to-training-mod-items.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.alterTable("Question", function (t) {
-        t.integer("order").nullable().defaultTo(null);
+        t.integer("order");
     });
 }
 
