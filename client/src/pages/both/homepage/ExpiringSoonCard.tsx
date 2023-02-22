b337f528be9007b15892db162aa0af267ff482f1
mattcorrea <islefan21@optonline.net>
2023-02-22 17:05:52 -0500

    Updated homepage

---
+++ client/src/pages/both/homepage/ExpiringSoonCard.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import Page from "../../Page";
+import Explainer from "../../maker/training/Explainer";
+import { gql, useQuery } from "@apollo/client";
+import RequestWrapper2 from "../../../common/RequestWrapper2";
+import { useCurrentUser } from "../../../common/CurrentUserProvider";
+import {
+    ModuleStatus,
+    moduleStatusMapper,
+} from "../../../common/TrainingModuleUtils";
+import TrainingModuleRow from "../../../common/TrainingModuleRow";
+import { searchFilter } from "../../../common/SearchBar";
+import {Card, Typography} from "@mui/material";
+
+export const GET_ALL_TRAINING_MODULES = gql`
+  query GetAllTrainingModules {
+    modules {
+      id
+      name
+    }
+  }
+`;
+
+export default function ExpiringSoon() {
+    const { passedModules } = useCurrentUser();
+    const result = useQuery(GET_ALL_TRAINING_MODULES);
+    const [searchText] = useState("");
+
+    return (
+        <RequestWrapper2
+            result={result}
+            render={({ modules }) => {
+                const moduleStatuses = modules.map(moduleStatusMapper(passedModules));
+
+                const matching = searchFilter<ModuleStatus>(
+                    searchText,
+                    moduleStatuses,
+                    (ms: ModuleStatus) => ms.moduleName
+                );
+
+                const expired = matching.filter(
+                    (ms: ModuleStatus) => ms.status === "Expired"
+                );
+
+                const reordered = [...expired];
+
+                return (
+                    <Card sx={{ width: 350, padding: 2, border: 1, borderColor: "lightgrey" }} >
+                        <Typography variant="h4">Expired Trainings</Typography>
+                        {reordered.map((ms: ModuleStatus) => (
+                            <TrainingModuleRow key={ms.moduleID} moduleStatus={ms} />
+                        ))}
+                    </Card>
+                );
+            }}
+        />
+    );
+}
