90e29c247c14a100c6d13a006ab36698b3d950dc
Charlton <cjl7292@rit.edu>
2023-06-09 11:56:12 -0400

    just put buttons in a button folder

---
+++ client/src/pages/lab_management/manage_equipment/buttons/PublishEquipmentButton.tsx
@@ -0,0 +1,36 @@
+import { useMutation } from "@apollo/client";
+import GET_EQUIPMENTS, { GET_ARCHIVED_EQUIPMENTS, GET_EQUIPMENT_BY_ID, PUBLISH_EQUIPMENT } from "../../../../queries/equipments";
+import { useNavigate } from "react-router-dom";
+import PublishButton from "../../../../common/PublishButton";
+
+interface PublishEquipmentButtonProps {
+  equipmentID: number;
+  appearance: "icon-only" | "small" | "medium" | "large"
+}
+
+export default function PublishEquipmentButton(props: PublishEquipmentButtonProps) {
+  const navigate = useNavigate();
+
+  const [publishEquipment, { loading }] = useMutation(PUBLISH_EQUIPMENT, {
+    variables: { id: props.equipmentID },
+    refetchQueries: [
+      { query: GET_EQUIPMENTS },
+      { query: GET_ARCHIVED_EQUIPMENTS },
+      { query: GET_EQUIPMENT_BY_ID, variables: { id: props.equipmentID } },
+    ],
+  });
+
+  const handleClick = async () => {
+    await publishEquipment();
+    navigate("/admin/equipment");
+  };
+
+  return (
+    <PublishButton
+      appearance={props.appearance}
+      handleClick={handleClick}
+      loading={loading}
+      tooltipText="Publish Equipment"
+    />
+  );
+}
