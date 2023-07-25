11f212dd3b9423f5f4514ba5940fce92d1dad306
Lollygagger <ubercharge60@gmail.com>
2023-07-25 15:06:49 -0400

    removed now ignored dist

--- server/dist/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MODULE_PASSING_THRESHOLD = void 0;
-exports.MODULE_PASSING_THRESHOLD = 80;
-//# sourceMappingURL=constants.js.map
--- server/dist/models/equipment/reservation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ReservationStatus;
-(function (ReservationStatus) {
-    ReservationStatus[ReservationStatus["PENDING"] = 0] = "PENDING";
-    ReservationStatus[ReservationStatus["CONFIRMED"] = 1] = "CONFIRMED";
-    ReservationStatus[ReservationStatus["CANCELLED"] = 2] = "CANCELLED";
-})(ReservationStatus || (ReservationStatus = {}));
-//# sourceMappingURL=reservation.js.map
