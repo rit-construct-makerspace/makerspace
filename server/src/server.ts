64a39c2c3601fef3715e25489bc384e47fdaa69f
Lollygagger <ubercharge60@gmail.com>
2023-07-31 15:11:13 -0400

    updated path.resolve to path.join

--- server/src/server.ts
+++ server/src/server.ts
@@ -70,7 +70,7 @@ async function startServer() {
   });
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../../client/build", 'index.html'));
+    res.sendFile(path.join(__dirname, "../../client/build", 'index.html'));
   });
 
   await server.start();
