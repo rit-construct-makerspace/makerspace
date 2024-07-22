ec331cb621edf00d405ecbeecf9b3c9f1bb11f9f
evstod <e.d.stoddard345@gmail.com>
2024-07-22 15:58:28 -0400

    Reader Card layout and styling

--- client/src/pages/lab_management/readers/ReaderCard.tsx
+++ client/src/pages/lab_management/readers/ReaderCard.tsx
@@ -1,14 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import {
   Card,
-  CardActionArea,
   CardContent,
   CardHeader,
-  CardMedia,
   IconButton,
   Typography,
 } from "@mui/material";
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { makeStyles } from '@material-ui/core/styles';
+import { GET_EQUIPMENT_BY_ID } from "../../../queries/equipmentQueries";
+import RequestWrapper from "../../../common/RequestWrapper";
+import AuditLogEntity from "../audit_logs/AuditLogEntity";
+import GET_ROOMS from "../../../queries/getRooms";
+import { useQuery } from "@apollo/client";
 
 interface ReaderCardProps {
     id: number,
@@ -25,86 +29,133 @@ interface ReaderCardProps {
     lastStatusTime: string
 }
 
-export default function ReaderCard({ id, machineID, machineType, name, zone, temp, state, currentUID, recentSessionLength, lastStatusReason, scheduledStatusFreq , lastStatusTime }: ReaderCardProps) {
-  const machineContent = machineID != undefined ? ", Machine: " + machineID : "";
+const useStyles = makeStyles({
+  errorText: {
+    color: "red",
+    fontWeight: "bold"
+  },
+  errorCard: {
+    border: "3px solid red"
+  }
+});
 
+export default function ReaderCard({ id, machineID, machineType, name, zone, temp, state, currentUID, recentSessionLength, lastStatusReason, scheduledStatusFreq , lastStatusTime }: ReaderCardProps) {
   const stateContent = state === "Active" ? (
     "Current User: " + currentUID + "\nSession Length" + recentSessionLength
   ) : (
     "Last Session Length: " + recentSessionLength
   );
 
+  const machineResult = useQuery(GET_EQUIPMENT_BY_ID, {
+    variables: { id }
+  });
+
+  const rooms = useQuery(GET_ROOMS);
+
+  const classes = useStyles();
+
   return (
-    <Card sx={{ width: 250 }}>
-      <CardHeader
-        action={
-          <IconButton aria-label="settings">
-              <MoreVertIcon />
-          </IconButton>
-          }
-          title={name}
-          subheader={machineType}
-      >
-      </CardHeader>
-      <CardContent>
-        <Typography
-          variant="body2"
-          component="div"
-          sx={{ lineHeight: 1, mb: 1 }}
-          noWrap
+    <RequestWrapper
+    loading={machineResult.loading}
+    error={machineResult.error}
+    >
+      <Card sx={{ width: 350 }} className={lastStatusReason == "Error" || lastStatusReason == "Temperature" ? classes.errorCard : ""}>
+        <CardHeader
+          action={
+            <IconButton aria-label="settings">
+                <MoreVertIcon />
+            </IconButton>
+            }
+            title={name}
+            subheader={"Type: " + machineType}
         >
-          Device ID: {id}, Zone(s): {zone}{machineContent}<br></br>
-        </Typography>
-        <Card variant="outlined">
-          <CardContent>
-              <Typography
-                  variant="h6"
-                  component="div"
-                  sx={{ lineHeight: 1, mb: 1 }}
-                  noWrap
-              >
-                  Temp
-              </Typography>
-              <Typography
-                  variant="h3"
-                  component="div"
-                  sx={{ lineHeight: 1, mb: 1 }}
-                  noWrap
-              >
-                  {temp}
-              </Typography>
-          </CardContent>
-        </Card>
-        <Card variant="outlined">
-          <CardContent>
-              <Typography
-                  variant="h6"
-                  component="div"
-                  sx={{ lineHeight: 1, mb: 1 }}
-                  noWrap
-              >
-                  {state == null ? "NULL" : state}
-              </Typography>
-              <Typography
-                  variant="h6"
-                  component="div"
-                  sx={{ lineHeight: 1, mb: 1 }}
-                  noWrap
-              >
-                  {stateContent}
-              </Typography>
-          </CardContent>
-              <Typography
-                  variant="body2"
-                  component="div"
-                  sx={{ lineHeight: 1, mb: 1 }}
-                  noWrap
-              >
-                  <b>Last Status:</b> {lastStatusTime == null ? "NULL" : lastStatusTime} - <b>Reason:</b> {lastStatusReason}<br></br>
-                  <b>Regular Status Interval:</b> {scheduledStatusFreq}
-              </Typography>
-        </Card>
-      </CardContent>
-    </Card>
+        </CardHeader>
+        <CardContent>
+          <Typography
+            variant="body2"
+            component="div"
+            sx={{ lineHeight: 1, mb: 1 }}
+          >
+            <b>Device ID: </b>{id}
+            <br></br>
+            <br></br>
+            <b>Zone(s): </b>
+            {
+              zone.split(",").map(function(zoneStr) {
+                const zoneNum = parseInt(zoneStr);
+                const code = rooms.data == undefined ? "0:none:none" : "room:" + zoneNum + ":" + rooms.data.rooms.find((room: { id: number; }) => room.id == zoneNum).name;
+                return (
+                  <div><AuditLogEntity entityCode={code}></AuditLogEntity><br></br></div>
+                )
+              })
+            }
+            <br></br>
+            <b>Machine: </b> <AuditLogEntity entityCode={machineID == undefined || machineResult.data == undefined ? "0:none:none" : "equipment:" + machineResult.data.equipment.id + ":" + machineResult.data.equipment.name}></AuditLogEntity>
+
+            <br></br>
+          </Typography>
+          <Card variant="outlined">
+            <CardContent>
+                <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ lineHeight: 1, mb: 1 }}
+                    noWrap
+                >
+                    Temp
+                </Typography>
+                <Typography
+                    variant="h3"
+                    component="div"
+                    sx={{ lineHeight: 1, mb: 1 }}
+                    align="center"
+                    noWrap
+                >
+                    {temp}
+                </Typography>
+            </CardContent>
+          </Card>
+          <Card variant="outlined">
+            <CardContent>
+            <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ lineHeight: 1, mb: 1 }}
+                    noWrap
+                >
+                    State
+                </Typography>
+                <Typography
+                    variant="h4"
+                    component="div"
+                    sx={{ lineHeight: 1, mb: 1 }}
+                    noWrap
+                    align="center"
+                >
+                    {state == null ? "NULL" : state}
+                </Typography>
+            </CardContent>
+          </Card>
+          <br />
+          <Typography
+              variant="body2"
+              component="div"
+              sx={{ lineHeight: 1, mb: 1 }}
+              noWrap
+          >
+              <b>Last Status:</b> {lastStatusTime == null ? "NULL" : lastStatusTime} - <b>Reason:</b> <span className={lastStatusReason == "Error" || lastStatusReason == "Temperature" ? classes.errorText : ""}>{lastStatusReason}</span><br></br>
+              <b>Regular Status Interval:</b> {scheduledStatusFreq}
+          </Typography>
+          <Typography
+              variant="body2"
+              component="div"
+              sx={{ lineHeight: 1, mb: 1 }}
+              noWrap
+          >
+              {stateContent}
+          </Typography>
+        </CardContent>
+      </Card>
+    </RequestWrapper>
   );
 }
