9a8e817b1a0c0b7c7258dc9cdb04036eb2593a02
evstod <e.d.stoddard345@gmail.com>
2024-07-24 14:31:26 -0400

    reenable accessChecks migration

--- server/src/db/migrations/20240723111717_access_checks.ts
+++ server/src/db/migrations/20240723111717_access_checks.ts
@@ -1,23 +1,24 @@
-// import { Knex } from "knex";
+import { Knex } from "knex";
 
-// export async function up(knex: Knex): Promise<void> {
-//   knex.schema.hasTable("AccessChecks").then(function (exists) {
-//     if (!exists) {
-//       return knex.schema.createTable("AccessChecks", function (t) {
-//         t.increments("id").primary();
-//         t.integer("userID").references("id").inTable("Users").notNullable();
-//         t.integer("equipmentID").references("id").inTable("Equipment").notNullable();
-//         t.date("readyDate").defaultTo(knex.fn.now());
-//         t.boolean("approved").defaultTo(false);
-//       });
-//     }
-//   });
-// }
+export async function up(knex: Knex): Promise<void> {
+  knex.schema.hasTable("AccessChecks").then(function (exists) {
+    if (!exists) {
+      console.log(exists);
+      return knex.schema.createTable("AccessChecks", function (t) {
+        t.increments("id").primary();
+        t.integer("userID").references("id").inTable("Users").notNullable();
+        t.integer("equipmentID").references("id").inTable("Equipment").notNullable();
+        t.date("readyDate").defaultTo(knex.fn.now());
+        t.boolean("approved").defaultTo(false);
+      });
+    }
+  });
+}
 
-// export async function down(knex: Knex): Promise<void> {
-//   knex.schema.hasTable("AccessChecks").then(function (exists) {
-//     if (exists) {
-//       return knex.schema.dropTable("AccessChecks");
-//     }
-//   });
-// }
+export async function down(knex: Knex): Promise<void> {
+  knex.schema.hasTable("AccessChecks").then(function (exists) {
+    if (exists) {
+      return knex.schema.dropTable("AccessChecks");
+    }
+  });
+}
