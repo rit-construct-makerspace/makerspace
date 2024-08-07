b38157683aa2eef5804b5f46400a5b16dfe3dfec
evstod <e.d.stoddard345@gmail.com>
2024-08-07 09:15:21 -0400

    prevent column collision on equipment images migration

--- server/src/db/migrations/20240731140400_equipment_images.ts
+++ server/src/db/migrations/20240731140400_equipment_images.ts
@@ -4,10 +4,12 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     knex.schema.hasTable("Equipment").then(function (exists) {
         if (!exists) return;
-
-        return knex.schema.alterTable("Equipment", function (t) {
-            t.string("imageUrl").nullable();
-        });
+        return knex.schema.hasColumn("Equipment", "imageUrl").then(function (exists) {
+            if (!exists) return;
+            return knex.schema.alterTable("Equipment", function (t) {
+                t.string("imageUrl").nullable();
+            });
+        })
     });
 }
 
