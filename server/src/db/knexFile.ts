b8cfae965be742f12878a627e97724f5d4bdc560
evstod <e.d.stoddard345@gmail.com>
2024-08-08 11:23:32 -0400

    fix old __dirname usage in knexFile

--- server/src/db/knexFile.ts
+++ server/src/db/knexFile.ts
@@ -1,5 +1,6 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
+const __dirname = import.meta.dirname;
 
 require("dotenv").config({ path: __dirname + "/./../../.env" });
 
