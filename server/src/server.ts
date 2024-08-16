aeeb461e45dbeff508c2ebb1e911b2fec458b3e7
evstod <e.d.stoddard345@gmail.com>
2024-08-16 15:06:04 -0400

    fix undefined uid when logging auth failure

--- server/src/server.ts
+++ server/src/server.ts
@@ -233,7 +233,7 @@ async function startServer() {
 
     //If user is not found, fail
     if (user == undefined) {
-      if (API_DEBUG_LOGGING) createLog("UID {conceal} failed to activate a machine with error '{error}'", {id: 0, label: req.body.ID}, {id: 406, label: "User does not exist"});
+      if (API_DEBUG_LOGGING) createLog("UID {conceal} failed to activate a machine with error '{error}'", {id: 0, label: req.query.id?.toString() ?? "undefined_uid"}, {id: 406, label: "User does not exist"});
       return res.status(406).json({
         "Type": "Authorization",
         "Machine": req.query.machine,
