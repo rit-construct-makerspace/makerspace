ed6c449e4323e44ffeef847ada9e8861c7ce855d
evstod <e.d.stoddard345@gmail.com>
2024-08-20 09:15:00 -0400

    Revert previous. Log only question responses

--- server/src/resolvers/trainingModuleResolver.ts
+++ server/src/resolvers/trainingModuleResolver.ts
@@ -261,10 +261,10 @@ const TrainingModuleResolvers = {
 
             if (submittedOptionIDsCorrect(correctOptionIDs, submittedOptionIDs)) {
               correct++;
-              choiceSummary.push({questionNum: question.id, questionText: question.text, correct: true, answers: args });
+              choiceSummary.push({questionNum: question.id, questionText: question.text, correct: true, answers: submittedOptions });
             } else {
               incorrect++;
-              choiceSummary.push({questionNum: question.id, questionText: question.text, correct: false, answers: args });
+              choiceSummary.push({questionNum: question.id, questionText: question.text, correct: false, answers: submittedOptions });
             }
               
           }
