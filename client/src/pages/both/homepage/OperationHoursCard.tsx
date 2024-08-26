0ab4eb80c15d48b57299055b01bdfd58e17bd2c4
evstod <e.d.stoddard345@gmail.com>
2024-08-26 09:30:31 -0400

    Set 8/25 hours to homepage

--- client/src/pages/both/homepage/OperationHoursCard.tsx
+++ client/src/pages/both/homepage/OperationHoursCard.tsx
@@ -6,11 +6,12 @@ import {
 export default function AccountBalanceCard() {
     return (
         <Card sx={{ width: 350, maxHeight: 150, padding: 2, border: 1, borderColor: "lightgrey", flexGrow: 1  }} >
-            <Typography variant="h4">Lab Hours (SUMMER)</Typography>
+            <Typography variant="h4">Lab Hours <Typography variant="h6">(Week of 8/25)</Typography></Typography>
             <Stack direction={"row"} spacing={2}>
                 <Typography color={"darkorange"} variant="h6">Monday - Friday</Typography>
                 <Typography align={"right"} variant="h6">9:00am - 5:00pm</Typography>
             </Stack>
+            <Typography variant="body1">(Closed Monday 7/2 for Labor Day)</Typography>
         </Card>
     );
 }
