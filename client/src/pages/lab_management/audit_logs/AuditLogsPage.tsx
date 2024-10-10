73be2635ef97b798b3a20e542f311c9a0abb3c25
evstod <e.d.stoddard345@gmail.com>
2024-10-10 15:47:51 -0400

    Fix advanced options mobile view

--- client/src/pages/lab_management/audit_logs/AuditLogsPage.tsx
+++ client/src/pages/lab_management/audit_logs/AuditLogsPage.tsx
@@ -214,7 +214,7 @@ export default function LogPage() {
               <Typography>
                 Type
               </Typography>
-              <Stack direction={"column"} flexWrap={"wrap"} height={"12.5em"}>
+              <Stack direction={"column"} flexWrap={isMobile ? "nowrap" : "wrap"} height={isMobile ? "auto" : "12.5em"}>
                 <FormControlLabel
                   label="Sign-In Attempts"
                   control={<Checkbox checked={filters.welcome} onChange={(e) => handleAdvSearchChange(e, "welcome")} />}
@@ -225,7 +225,7 @@ export default function LogPage() {
                 />
                 <FormControlLabel
                   label="ACS Status Reports"
-                  control={<Checkbox checked={filters.state} onChange={(e) => handleAdvSearchChange(e, "state")} />}
+                  control={<Checkbox checked={filters.status} onChange={(e) => handleAdvSearchChange(e, "status")} />}
                 />
                 <FormControlLabel
                   label="ACS State Changes"
