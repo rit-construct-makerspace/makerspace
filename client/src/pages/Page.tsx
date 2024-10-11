e3da62fbd4c594ed72860ef65bcda5c2e4b14949
evstod <e.d.stoddard345@gmail.com>
2024-10-11 14:49:05 -0400

    Remove X scroll on mobile pages

--- client/src/pages/Page.tsx
+++ client/src/pages/Page.tsx
@@ -33,7 +33,7 @@ export default function Page({
   return (
     <Stack
       alignItems="center"
-      sx={{ width: "100%", height: "100vh", overflowY: "auto" }}
+      sx={{ width: "100%", height: "100vh", overflowY: "auto", overflowX: "hidden" }}
     >
       <Stack
         sx={{
