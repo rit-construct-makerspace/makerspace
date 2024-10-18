19416a4b168f5b2081635052b883c338f00db063
evstod <e.d.stoddard345@gmail.com>
2024-10-18 15:48:43 -0400

    Log access check un/approvals

--- server/src/resolvers/accessChecksResolver.ts
+++ server/src/resolvers/accessChecksResolver.ts
@@ -2,6 +2,9 @@ import * as EquipmentRepo from "../repositories/Equipment/EquipmentRepository.js
 import { Privilege } from "../schemas/usersSchema.js";
 import { ApolloContext } from "../context.js";
 import { getAccessCheckByID, getAccessChecks, getAccessChecksByApproved, setAccessCheckApproval } from "../repositories/Equipment/AccessChecksRepository.js";
+import { createLog } from "../repositories/AuditLogs/AuditLogRepository.js";
+import { getUserByID, getUsersFullName } from "../repositories/Users/UserRepository.js";
+import { GraphQLError } from "graphql";
 const AccessChecksResolver = {
 
   Query: {
@@ -43,7 +46,15 @@ const AccessChecksResolver = {
       _parent: any,
       args: { id: number },
       { ifAllowed }: ApolloContext) =>
-      ifAllowed([Privilege.MENTOR, Privilege.STAFF], async () => {
+      ifAllowed([Privilege.MENTOR, Privilege.STAFF], async (user) => {
+        const check = await getAccessCheckByID(args.id);
+        if (!check) throw new GraphQLError("Access Check does not exist");
+        const equipment = await EquipmentRepo.getEquipmentByID(check?.equipmentID);
+        if (!equipment) throw new GraphQLError("Equipment does not exist");
+        const affectedUser = await getUserByID(check.userID);
+        if (!affectedUser) throw new GraphQLError("User does not exist");
+        await createLog(`{user} approved the {equipment} access check for {user}`, `admin`, 
+          {id: user.id, label: getUsersFullName(user)}, {id: equipment.id, label: equipment.name}, {id: affectedUser.id, label: getUsersFullName(affectedUser)});
         return await setAccessCheckApproval(args.id, true);
       }),
 
@@ -51,7 +62,15 @@ const AccessChecksResolver = {
       _parent: any,
       args: { id: number },
       { ifAllowed }: ApolloContext) =>
-      ifAllowed([Privilege.MENTOR, Privilege.STAFF], async () => {
+      ifAllowed([Privilege.MENTOR, Privilege.STAFF], async (user) => {
+        const check = await getAccessCheckByID(args.id);
+        if (!check) throw new GraphQLError("Access Check does not exist");
+        const equipment = await EquipmentRepo.getEquipmentByID(check?.equipmentID);
+        if (!equipment) throw new GraphQLError("Equipment does not exist");
+        const affectedUser = await getUserByID(check.userID);
+        if (!affectedUser) throw new GraphQLError("User does not exist");
+        await createLog(`{user} unapproved the {equipment} access check for {user}`, `admin`, 
+          {id: user.id, label: getUsersFullName(user)}, {id: equipment.id, label: equipment.name}, {id: affectedUser.id, label: getUsersFullName(affectedUser)});
         return await setAccessCheckApproval(args.id, false);
       }),
   }
