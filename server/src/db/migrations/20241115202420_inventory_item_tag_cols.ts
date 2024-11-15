666f4ff2d28c7a77319a877dfbc213c9b89ecb9b
evstod <e.d.stoddard345@gmail.com>
2024-11-15 15:34:55 -0500

    Inventory Tag Table

---
+++ server/src/db/migrations/20241115202420_inventory_item_tag_cols.ts
@@ -0,0 +1,29 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    knex.schema.hasTable("InventoryItem").then(function (exists) {
+        if (!exists) return;
+        knex.schema.hasColumn("InventoryItem", "tagID1").then(function(c) {
+            if (c) return;
+            return knex.schema.alterTable("MaintenanceLogs", function (t) {
+                    t.integer("tagID1").references("id").inTable("InventoryTags");
+                    t.integer("tagID2").references("id").inTable("InventoryTags");
+                    t.integer("tagID3").references("id").inTable("InventoryTags");
+                });
+            });
+        })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    knex.schema.hasTable("InventoryItem").then(function (exists) {
+        if (!exists) return;
+        return knex.schema.alterTable("InventoryItem", function (t) {
+            t.dropColumn("tagID1");
+            t.dropColumn("tagID2");
+            t.dropColumn("tagID3");
+        });
+    });
+}
+
