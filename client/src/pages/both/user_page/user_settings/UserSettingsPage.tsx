f039d51550ffd4cf479a5ede8dbbd73cca776b1d
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-05-13 13:45:36 -0400

    Added content to User Settings page

--- client/src/pages/both/user_page/user_settings/UserSettingsPage.tsx
+++ client/src/pages/both/user_page/user_settings/UserSettingsPage.tsx
@@ -1,6 +1,54 @@
-import { useParams } from "react-router-dom";
+import { useCurrentUser } from "../../../../common/CurrentUserProvider";
+import { Alert, Avatar, Chip, Divider, Stack, TextField, Typography } from "@mui/material";
+import { useQuery } from "@apollo/client";
+import { GET_USER } from "../../../lab_management/users/UserModal";
+import RequestWrapper from "../../../../common/RequestWrapper";
+import InfoBlob from "../../../lab_management/users/InfoBlob";
+import styled from "styled-components";
+
+const StyledInfo = styled.div`
+  margin-top: 16px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  row-gap: 32px;
+  width: 50%;
+`;
 
 
 export default function UserSettingsPage() {
-    const { id } = useParams<{id: string}>();
+    const currentUser = useCurrentUser();
+
+    const userResult = useQuery(GET_USER, {variables: {id: currentUser.id}});
+
+    return (
+        <Stack margin="30px" width="100%" spacing={2} divider={<Divider orientation="horizontal" flexItem/>}>
+            {/* Personal info */}
+            <RequestWrapper loading={userResult.loading} error={userResult.error}>
+                <Stack spacing={1}>
+                    <Stack direction="row" spacing={2} alignItems="center">
+                        <Avatar
+                            alt="Profile picture"
+                            src="https://t4.ftcdn.net/jpg/00/64/67/63/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+                            sx={{width: "60px", height: "60px"}}
+                        />
+                        <Typography variant="h3">{userResult.data?.user.firstName} {userResult.data?.user.lastName} ({userResult.data?.user.ritUsername})</Typography>
+                    </Stack>
+                    <StyledInfo>
+                        <InfoBlob label="Pronous" value={userResult.data?.user.pronouns}/>
+                        <InfoBlob label="Role" value={userResult.data?.user.privilege}/>
+                    </StyledInfo>
+                    <StyledInfo>
+                        <InfoBlob label="College" value={userResult.data?.user.college}/>
+                        <InfoBlob label="Expected Graduation" value={userResult.data?.user.expectedGraduation}/>
+                    </StyledInfo>
+                    <InfoBlob label="Member Since" value={userResult.data?.user.registrationDate}/>
+                </Stack>
+            </RequestWrapper>
+            {/* Appearence (Dark Mode toggle) */}
+            <Stack spacing={1}>
+                <Typography variant="h3">Appearance</Typography>
+                <Typography sx={{fontStyle: "italic"}}>Under Construction...</Typography>
+            </Stack>
+        </Stack>
+    );
 }
---
+++ client/src/pages/both/user_page/user_trainings/UserTrainingsPage.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "@mui/material";
+
+export default function UserTraingingsPage() {
+    
+    return (
+        <Stack>
+            <h1>wip</h1>
+        </Stack>
+    );
+}
