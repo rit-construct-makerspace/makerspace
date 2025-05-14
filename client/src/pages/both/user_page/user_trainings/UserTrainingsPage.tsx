8878b2fceb9cf16e46c713542dbcdfa595df9217
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-05-14 08:46:00 -0400

    User approved equipment

--- client/src/pages/both/user_page/user_trainings/UserTrainingsPage.tsx
+++ client/src/pages/both/user_page/user_trainings/UserTrainingsPage.tsx
@@ -1,5 +1,5 @@
 import { Divider, Stack, Typography } from "@mui/material";
-import { useCurrentUser } from "../../../../common/CurrentUserProvider";
+import { PassedModule, useCurrentUser } from "../../../../common/CurrentUserProvider";
 import { GET_ALL_TRAINING_MODULES } from "../../../maker/training/TrainingPage";
 import { useQuery } from "@apollo/client";
 import RequestWrapper from "../../../../common/RequestWrapper";
@@ -7,11 +7,14 @@ import { searchFilter } from "../../../../common/SearchBar";
 import { ModuleStatus, moduleStatusMapper } from "../../../../common/TrainingModuleUtils";
 import TrainingModuleRow from "../../../../common/TrainingModuleRow";
 import RequestWrapper2 from "../../../../common/RequestWrapper2";
+import GET_EQUIPMENTS from "../../../../queries/equipmentQueries";
+import Equipment from "../../../../types/Equipment";
 
 export default function UserTraingingsPage() {
-    const { passedModules, trainingHolds } = useCurrentUser();
+    const user = useCurrentUser();
 
     const getAllModules = useQuery(GET_ALL_TRAINING_MODULES);
+    const getEquipments = useQuery(GET_EQUIPMENTS);
 
     return (
         <Stack spacing={2} margin="20px" width="100%" divider={<Divider orientation="horizontal" flexItem/>}>
@@ -19,24 +22,19 @@ export default function UserTraingingsPage() {
             <RequestWrapper2
                 result={getAllModules}
                 render={({modules}) => {
-                    const moduleStatuses = modules.map(moduleStatusMapper(passedModules, trainingHolds));
+                    const moduleStatuses = modules.map(moduleStatusMapper(user.passedModules, user.trainingHolds));
                     
-                    const matching = searchFilter<ModuleStatus>(
-                        "",
-                        moduleStatuses,
-                        (ms: ModuleStatus) => ms.moduleName
-                    );
             
-                    const expired = matching.filter(
+                    const expired = moduleStatuses.filter(
                         (ms: ModuleStatus) => ms.status === "Expired"
                     );
-                    const passed = matching.filter(
+                    const passed = moduleStatuses.filter(
                         (ms: ModuleStatus) => ms.status === "Passed"
                     );
-                    const notTaken = matching.filter(
+                    const notTaken = moduleStatuses.filter(
                         (ms: ModuleStatus) => ms.status === "Not taken"
                     );
-                    const locked = matching.filter(
+                    const locked = moduleStatuses.filter(
                         (ms: ModuleStatus) => ms.status === "Locked"
                     );
 
@@ -45,7 +43,7 @@ export default function UserTraingingsPage() {
                             spacing={3}
                             direction="row"
                             divider={<Divider orientation="vertical" flexItem/>}
-                            maxHeight="25%"
+                            maxHeight="30%"
                         >
                             {/* Complete Trainings */}
                             <Stack width="33%" overflow="auto">
@@ -73,6 +71,38 @@ export default function UserTraingingsPage() {
                 }}
             />
             {/* Equipment */}
+            <RequestWrapper2
+                result={getEquipments}
+                render={({equipments}) => {
+
+                    equipments.filter((m: Equipment) => {
+                        const moduleStatuses: ModuleStatus[] = m.trainingModules.map(
+                            moduleStatusMapper(user.passedModules, user.trainingHolds)
+                        );
+
+                        moduleStatuses.map((status: ModuleStatus) => {
+                            user.passedModules.forEach((passed: PassedModule) => {
+                                status.moduleID === passed.moduleID
+                                return true;
+                            })
+                            return false;
+                        })
+                    }).map(e: Equipment)
+
+                    return (
+                        <Stack spacing={1} maxHeight="30%">
+                            <Typography variant="h4">Approved Equipment</Typography>
+                            <Stack direction="row" overflow="auto">
+                                
+                            </Stack>
+                            <Typography variant="h4">Awaiting In-person Knowledge Check</Typography>
+                            <Stack direction="row" overflow="auto">
+                            
+                            </Stack>
+                        </Stack>
+                    );
+                }}
+            />
             {/* History */}
         </Stack>
     );
