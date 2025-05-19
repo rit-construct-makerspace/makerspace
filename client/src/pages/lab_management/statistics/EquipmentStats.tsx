fb40d931582bcfc27bcb17416f5a105d2389816f
evstod <e.d.stoddard345@gmail.com>
2025-05-19 10:07:13 -0400

    enforce string type before substring call on secondsToHumanString

--- client/src/pages/lab_management/statistics/EquipmentStats.tsx
+++ client/src/pages/lab_management/statistics/EquipmentStats.tsx
@@ -50,7 +50,7 @@ function secondsToHumanString (seconds: number) {
 
     for (var i = 0, max = levels.length; i < max; i++) {
         if ( levels[i][0] === 0 ) continue;
-        returntext += ' ' + levels[i][0] + ' ' + (levels[i][0] === 1 ? levels[i][1].substr(0, levels[i][1].length-1): levels[i][1]);
+        returntext += ' ' + levels[i][0] + ' ' + (levels[i][0] === 1 ? levels[i][1].toString().substring(0, levels[i][1].toString().length-1): levels[i][1]);
     };
     return returntext.trim();
 }
