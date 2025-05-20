311ff1ce63fb83fc8fe05777469f9f65783236a5
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-05-20 12:24:56 -0400

    Added instances to manage equipment page

--- client/src/pages/lab_management/manage_equipment/EquipmentEditor.tsx
+++ client/src/pages/lab_management/manage_equipment/EquipmentEditor.tsx
@@ -1,6 +1,6 @@
-import { ChangeEvent, ChangeEventHandler, SetStateAction, SyntheticEvent, useState } from "react";
+import { ChangeEvent, ChangeEventHandler, SetStateAction, SyntheticEvent, useEffect, useState } from "react";
 import Page from "../../Page";
-import { Autocomplete, Button, Divider, FormControlLabel, Stack, Switch, TextareaAutosize, TextField } from "@mui/material";
+import { Autocomplete, Button, Divider, FormControlLabel, Stack, Switch, TextareaAutosize, TextField, Typography } from "@mui/material";
 import EngineeringIcon from "@mui/icons-material/Engineering";
 import HistoryIcon from "@mui/icons-material/History";
 import PageSectionHeader from "../../../common/PageSectionHeader";
@@ -19,6 +19,8 @@ import SpeakerNotesIcon from '@mui/icons-material/SpeakerNotes';
 import { useNavigate } from "react-router-dom";
 import EquipmentInstancesModal from "./EquipmentInstancesModal";
 import AutoAwesomeMotionIcon from '@mui/icons-material/AutoAwesomeMotion';
+import { EquipmentInstance, GET_EQUIPMENT_INSTANCES } from "../../../queries/equipmentInstanceQueries";
+import EquipmentInstanceRow from "./EquipmentInstanceRow";
 
 const StyledMachineImage = styled.img`
   width: 128px;
@@ -111,128 +113,146 @@ export default function EquipmentEditor({
 
   const [instancesModalOpen, setInstancesModalOpen] = useState<boolean>(false);
 
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  function handleWindowSizeChange() {
+      setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+      window.addEventListener('resize', handleWindowSizeChange);
+      return () => {
+          window.removeEventListener('resize', handleWindowSizeChange);
+      }
+  }, []);
+
+  const isMobile = width <= 1100;
+
+  const equipmentInstancesResult = useQuery(GET_EQUIPMENT_INSTANCES, {variables: {equipmentID: equipment.id}});
+
   return (
     <RequestWrapper
-      loading={getRoomsResult.loading || getModulesResult.loading}
-      error={getRoomsResult.error || getModulesResult.error}
+      loading={getRoomsResult.loading || getModulesResult.loading || equipmentInstancesResult.loading}
+      error={getRoomsResult.error || getModulesResult.error || equipmentInstancesResult.error}
     >
-      <AdminPage
-        title={`${newEquipment ? "Create New" : "Manage"} Equipment`}
-      >
-        {!newEquipment && (
-          <Stack direction="row" spacing={1} sx={{ mt: -2, mb: 4 }}>
-            <Button variant="outlined" startIcon={<HistoryIcon />}>
-              View Logs
-            </Button>
+      <AdminPage>
+        <Stack padding="15px">
+          <Stack direction="row" justifyContent="space-between" alignItems="baseline">
+            <Typography variant="h3">{`${newEquipment ? "Create New" : "Manage"} Equipment`}</Typography>
             {
-              equipment.id ?
-                equipment.archived
-                  ? <PublishEquipmentButton equipmentID={equipment.id} appearance="medium" /> 
-                  : <ArchiveEquipmentButton equipmentID={equipment.id} appearance="medium" />
-                : null
+              !newEquipment
+              ? <Stack direction="row" spacing={1}>
+                {
+                  equipment.id ?
+                    equipment.archived
+                      ? <PublishEquipmentButton equipmentID={equipment.id} appearance="medium" /> 
+                      : <ArchiveEquipmentButton equipmentID={equipment.id} appearance="medium" />
+                    : null
+                }
+                {equipment.id && <Button variant="outlined" color="secondary" startIcon={<SpeakerNotesIcon />} onClick={() => {navigate("/admin/equipment/issues/" + equipment.id);}}>
+                  View Issue Logs
+                </Button>}
+              </Stack>
+              : null
             }
-            {equipment.id && <Button variant="outlined" color="secondary" startIcon={<SpeakerNotesIcon />} onClick={() => {navigate("/admin/equipment/issues/" + equipment.id);}}>
-              View Issue Logs
-            </Button>}
-            {equipment.id && <Button variant="outlined" color="info" startIcon={<AutoAwesomeMotionIcon />} onClick={() => {setInstancesModalOpen(true)}}>
-              Manage Instances
-            </Button>}
           </Stack>
-        )}
+          
+          <Stack
+            direction={isMobile ? "column" : "row"}
+            spacing={2}
+            width="auto"
+            padding="0px 10px"
+          >
+            <Stack spacing={2} width="50%">
+              <Typography variant="h5">Machine Information</Typography>
+              <TextField
+                label="Name"
+                value={equipment.name}
+                onChange={handleNameChanged}
+                inputProps={{
+                  maxLength: 50
+                }}
+              />
+              <TextField
+                label="Image URL"
+                value={equipment.imageUrl}
+                onChange={handleImageChanged}
+              />
+              <TextField
+                label="SOP URL"
+                value={equipment.sopUrl}
+                onChange={handleSopChanged}
+              />
+              <Autocomplete
+                renderInput={(params: any) => (
+                  <TextField {...params} label="Location" />
+                )}
+                /* Autocomplete's value prop wants undefined, not null.
+                * But if we give it undefined then it thinks it's an
+                * uncontrolled prop and throws a console error
+                * when we set the value. This is a MUI problem.
+                * @ts-ignore */
+                value={equipment.room}
+                options={getRoomsResult.data?.rooms ?? []}
+                isOptionEqualToValue={(option, value) => option.id === value.id}
+                getOptionLabel={(option) => option.name}
+                onChange={handleRoomChanged}
+                disableClearable
+              />
+              <TextField
+                label="Description"
+                style={{background: "none", fontFamily: "Roboto", fontSize: "1em", lineHeight: "2em"}}
+                aria-label="Description" 
+                defaultValue={equipment.notes} 
+                placeholder="Description (Markdown Compatible)"
+                onChange={handleNotesChanged}
+                rows={3}
+              />
+              <FormControlLabel control={<Switch checked={equipment.byReservationOnly} onChange={handleByReservationOnlySwitchChanged} />} label={"Available by reservation only"} />
+              <Typography variant="h5">Training Modules</Typography>
+              <Stack divider={<Divider flexItem />} spacing={1}>
+                {equipment.trainingModules.map((m) => (
+                  <AttachedModule
+                    module={m}
+                    key={m.id}
+                    onRemove={handleModuleRemoved(m.id)}
+                  />
+                ))}
+              </Stack>
+              <Autocomplete
+                key={equipment.trainingModules.length}
+                renderOption={(params, module) => (
+                  <li {...params} key={module.id}>
+                    {module.name}
+                  </li>
+                )}
+                renderInput={(params: any) => (
+                  <TextField {...params} label="Attach module" key={module.id} />
+                )}
+                options={getModuleOptions()}
+                isOptionEqualToValue={(option, value) => option.id === value.id}
+                getOptionLabel={(option) => option.name}
+                onChange={handleModuleAdded}
+              />
 
-        <PageSectionHeader top>Information</PageSectionHeader>
-        
-        <Stack direction="row" spacing={2}>
-          <StyledMachineImage
-            alt="Machine image"
-            src={(equipment.imageUrl == undefined || equipment.imageUrl == null ? process.env.PUBLIC_URL + "/shed_acronym_vert.jpg" : "" + process.env.REACT_APP_CDN_URL + process.env.REACT_APP_CDN_EQUIPMENT_DIR + "/" + equipment.imageUrl)}
-          />
-          <Stack spacing={2} flexGrow={1}>
-            <TextField
-              label="Name"
-              value={equipment.name}
-              onChange={handleNameChanged}
-              inputProps={{
-                maxLength: 50
-              }}
-            />
-            <TextField
-              label="Image URL"
-              value={equipment.imageUrl}
-              onChange={handleImageChanged}
-            />
-            <TextField
-              label="SOP URL"
-              value={equipment.sopUrl}
-              onChange={handleSopChanged}
-            />
-            <Autocomplete
-              renderInput={(params: any) => (
-                <TextField {...params} label="Location" />
-              )}
-              /* Autocomplete's value prop wants undefined, not null.
-               * But if we give it undefined then it thinks it's an
-               * uncontrolled prop and throws a console error
-               * when we set the value. This is a MUI problem.
-               * @ts-ignore */
-              value={equipment.room}
-              options={getRoomsResult.data?.rooms ?? []}
-              isOptionEqualToValue={(option, value) => option.id === value.id}
-              getOptionLabel={(option) => option.name}
-              onChange={handleRoomChanged}
-              disableClearable
-            />
-            <TextField
-              label="Description"
-              style={{background: "none", fontFamily: "Roboto", fontSize: "1em", lineHeight: "2em"}}
-              aria-label="Description" 
-              defaultValue={equipment.notes} 
-              placeholder="Description (Markdown Compatible)"
-              onChange={handleNotesChanged}
-              rows={3}
-            />
-            <FormControlLabel control={<Switch checked={equipment.byReservationOnly} onChange={handleByReservationOnlySwitchChanged} />} label={"Available by reservation only? (Hides ACS availability from users)"} />
+              <Button
+                variant="contained"
+                size="large"
+                sx={{ mt: 4, alignSelf: "flex-end" }}
+                onClick={onSave}
+                color="success"
+              >
+                Save
+              </Button>
+            </Stack>
+            <Stack width="50%" spacing={2}>
+              <Typography variant="h5">Instances</Typography>
+              {
+                equipmentInstancesResult.data?.equipmentInstances.map((instance: EquipmentInstance) => (
+                  <EquipmentInstanceRow instance={instance} />
+                ))
+              }
+            </Stack>
           </Stack>
         </Stack>
-
-        <PageSectionHeader>Training Modules</PageSectionHeader>
-
-        <Stack divider={<Divider flexItem />} spacing={1}>
-          {equipment.trainingModules.map((m) => (
-            <AttachedModule
-              module={m}
-              key={m.id}
-              onRemove={handleModuleRemoved(m.id)}
-            />
-          ))}
-        </Stack>
-
-        <Autocomplete
-          key={equipment.trainingModules.length}
-          renderOption={(params, module) => (
-            <li {...params} key={module.id}>
-              {module.name}
-            </li>
-          )}
-          renderInput={(params: any) => (
-            <TextField {...params} label="Attach module" key={module.id} />
-          )}
-          options={getModuleOptions()}
-          isOptionEqualToValue={(option, value) => option.id === value.id}
-          getOptionLabel={(option) => option.name}
-          onChange={handleModuleAdded}
-        />
-
-        <Button
-          variant="contained"
-          size="large"
-          sx={{ mt: 4, alignSelf: "flex-end" }}
-          onClick={onSave}
-        >
-          Save
-        </Button>
-
-        {equipment.id && <EquipmentInstancesModal equipmentID={equipment.id} equipmentName={equipment.name} isOpen={instancesModalOpen} setIsOpen={setInstancesModalOpen} />}
       </AdminPage>
     </RequestWrapper>
   );
