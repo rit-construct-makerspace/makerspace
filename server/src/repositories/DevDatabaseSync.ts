14e2f4e3e6dab1a0ebe1418450e9d705d49deed7
Richard Sommers <44383226+cowsed@users.noreply.github.com>
2025-05-22 14:48:11 -0400

    Copy foreign keys into development database (#567)

--- server/src/repositories/DevDatabaseSync.ts
+++ server/src/repositories/DevDatabaseSync.ts
@@ -1,32 +1,60 @@
 import { knex } from "../db/index.js";
 
 const script: string = `
-CREATE OR REPLACE FUNCTION clone_schema(source_schema text, dest_schema text) RETURNS void AS
+create or replace function create_table_like(source_table text, new_table text)
+returns void language plpgsql
+as $$
+declare
+    rec record;
+begin
+--	RAISE NOTICE format('Creating table %s', new_table);
+--	RAISE NOTICE ('creating table' || source_table || ' to ' || new_table);
+    execute format(
+        'create table %s (like %s including all)',
+        new_table, source_table);
+    for rec in
+        select oid, conname
+        from pg_constraint
+        where contype = 'f'
+        and conrelid = source_table::regclass
+    loop
+		RAISE NOTICE 'Cpmstaraint';
+
+        execute format(
+            'alter table %s add constraint %s %s',
+            new_table,
+            replace(rec.conname, source_table, new_table),
+            pg_get_constraintdef(rec.oid));
+    end loop;
+end $$;
+
+create or replace function clone_schema(source_schema text, dest_schema text) RETURNS void AS
 $BODY$
-DECLARE 
+declare
   objeto text;
   buffer text;
   src_buffer text;
-BEGIN
-	RAISE NOTICE 'Copying Prod schema to dev';
-	EXECUTE 'DROP SCHEMA IF EXISTS ' || dest_schema || ' cascade';
-    EXECUTE 'CREATE SCHEMA if not exists ' || dest_schema ;
+begin
+	raise notice 'Copying Prod schema to dev';
+	execute 'DROP SCHEMA IF EXISTS ' || dest_schema || ' cascade';
+    execute 'CREATE SCHEMA if not exists ' || dest_schema ;
 
-    FOR objeto IN
-        SELECT table_name FROM information_schema.tables WHERE table_schema = source_schema and table_type = 'BASE TABLE'
-    LOOP        
+    for objeto in
+        select table_name from information_schema.tables where table_schema = source_schema and table_type = 'BASE TABLE'
+    loop
         buffer := dest_schema || '."' || objeto || '"';
 		src_buffer := source_schema || '."' || objeto ||'"';
 
-    	EXECUTE 'CREATE TABLE ' || buffer || ' (LIKE ' || src_buffer || ' INCLUDING all)';
-        EXECUTE 'INSERT INTO ' || buffer || '(SELECT * FROM ' || src_buffer || ')';
-    END LOOP;
+    	perform create_table_like(src_buffer, buffer);
+    end loop;
+	raise notice 'Finished';
 
-END;
+end;
 $BODY$
-LANGUAGE plpgsql VOLATILE;
+language plpgsql volatile;
 
-SELECT clone_schema('public','dev');
+select clone_schema('public','dev');
+
 `
 
 
