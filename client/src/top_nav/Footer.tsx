35d9901461e44aa2f8f3776d85ded890b3030677
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-05-22 11:33:44 -0400

    Added balloon ritchie to the footer

--- client/src/top_nav/Footer.tsx
+++ client/src/top_nav/Footer.tsx
@@ -8,11 +8,12 @@ export default function Footer() {
     return (
         <Stack marginTop="auto" justifyContent="flex-end">
             <AppBar position="static">
-                <Stack direction="row" width="auto" padding="25px">
-                    <Stack>
+                <Stack direction="row" width="auto" padding="20px" justifyContent="space-between" alignItems="center">
+                    <Stack spacing={1}>
                         <Typography variant="h4">Make Something Interesting</Typography>
                         <Typography color="inherit">Contact Us: <Link href="mailto:make@rit.edu" underline="hover" color="inherit">make@rit.edu</Link></Typography>
                     </Stack>
+                    <img src="https://emoji.slack-edge.com/T018A0NHZNY/balloonritchie/41333b3f01b96f1a.png" alt="Balloon Ritchie" height="60px"/>
                 </Stack>
             </AppBar>
         </Stack>
