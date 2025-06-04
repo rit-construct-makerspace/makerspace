1a3fb4d1c418e36c8fcaadc77e6baa65e5a81e2b
cowsed <richie.sommers@outlook.com>
2025-06-04 08:40:40 -0400

    always underline link

--- client/src/top_nav/Footer.tsx
+++ client/src/top_nav/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
                     <Stack spacing={1}>
                         <Typography variant="h4">Make Something Interesting</Typography>
                         <Typography color="inherit">Contact Us: <Link href="mailto:make@rit.edu" underline="hover" color="inherit">make@rit.edu</Link></Typography>
-                        <Typography variant="body1">This website uses cookies to provide better user experience and functionality. You can control and configure cookies in your web browser. <Link href="https://www.rit.edu/cookie-statement" underline="hover" color="inherit" >Cookie Statement.</Link></Typography>
+                        <Typography variant="body1">This website uses cookies to provide better user experience and functionality. You can control and configure cookies in your web browser. <Link href="https://www.rit.edu/cookie-statement" underline="always" color="inherit" >Cookie Statement.</Link></Typography>
                     </Stack>
                     <img src="https://emoji.slack-edge.com/T018A0NHZNY/balloonritchie/41333b3f01b96f1a.png" alt="Balloon Ritchie" height="60px"/>
                 </Stack>
